a5509063074af3518895e13c573c8a2470822681
pranav <pranav0492@gmail.com>
2025-02-15 12:14:30 +0000

    setup a pnpm monorepo with svelte and koa

---
+++ apps/frontend/src/routes/+page.server.ts
@@ -0,0 +1,9 @@
+// src/routes/+page.server.ts
+export async function load({ fetch }: { fetch: typeof window.fetch }) {
+	const res = await fetch('http://localhost:4000/api');
+	const data = await res.json();
+
+	return {
+		message: data.message
+	};
+}
---
+++ apps/frontend/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<!-- src/routes/+page.svelte -->
+<script>
+	let { data } = $props();
+</script>
+
+<h1>{data.message}</h1>
